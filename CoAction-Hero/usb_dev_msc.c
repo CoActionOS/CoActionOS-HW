e22576c850987860886127e283f363af75fed377
tyler-gilbert <tgil@coactionos.com>
2014-07-31 11:55:26 -0600

    Started work on MSC

---
+++ CoAction-Hero/usb_dev_msc.c
@@ -0,0 +1,15 @@
+/* Copyright 2011; All Rights Reserved
+ * Please see http://www.coactionos.com/license.html for
+ * licensing information.
+ */
+
+#include <string.h>
+#include <stdio.h>
+
+#include <usb_dev_msc.h>
+
+int usb_dev_msc_if_req(int event){
+
+	//The request was not handled
+	return 0;
+}
